f9f128d21b14acf20cc0b039788d7cc624bacd0f
aymendhieb1 <aymen.dhieb@esprit.tn>
2024-04-25 13:15:31 +0100

    Model_v0

---
+++ Model/education.php
@@ -0,0 +1,35 @@
+<?php
+
+class education{
+
+    private int $ID_EDUCATION;
+    private string $photo;
+    private string $Etat;
+    private string $emplacement;
+    private string $ID_Diplome_E;
+
+    public function  __construct() {
+        $this->ID_EDUCATION=0;
+        $this->photo="";
+        $this->Etat="";
+        $this->emplacement="";
+        $this->ID_Diplome_E="";
+    }
+    public function getID_EDUCATION() {return  $this->ID_EDUCATION;}
+    public function setID_EDUCATION($n) {$this->ID_EDUCATION =$n ;}
+
+    public function getphoto() {return  $this->photo;}
+    public function setphoto($n) {$this->photo =$n ;}
+    public function getEtat(){ return   $this->Etat;}  
+    public function setEtat($p){ $this->Etat = $p;}
+    public function getemplacement() {return $this->emplacement;}
+    public function setemplacement($e) {$this->emplacement = $e;}
+    public function getID_Diplome_E() {return  $this->ID_Diplome_E;}
+    public function setID_Diplome_E($n) {$this->ID_Diplome_E =$n ;}
+
+
+}
+
+
+
+?>
