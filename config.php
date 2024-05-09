2e6f0b2a9bdcbb04216d916fd671bb27b0444974
nadi202 <nadine.chrigui@esprit.tn>
2024-05-09 14:04:39 +0100

    config

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+class config{
+    private static $pdo = NULL;
+    public static function getConnexion()
+    {
+        if (!isset(self::$pdo)) {
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "projet";
+            try {
+                self::$pdo = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+                );
+            } catch (Exception $e) {
+                die('Erreur: ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+config::getConnexion();
+
+?>
